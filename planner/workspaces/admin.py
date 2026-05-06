3c8ca82e781f7598c14bd980352cb412f1063b6f
duskrosee <serpente.mari@gmail.com>
2026-05-06 18:25:42 +0200

    Delete planner directory

--- planner/workspaces/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Workspace
-
-
-@admin.register(Workspace)
-class WorkspaceAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'created_at')
-    search_fields = ('name', 'owner__username')
